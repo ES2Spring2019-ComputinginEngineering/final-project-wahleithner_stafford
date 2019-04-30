e9b371b780cc37f5e7b0f4285884be01f55003bb
Gillian Wahleithner <gillian.wahleithner@gmail.com>
2019-04-30 13:02:05 -0400

    Delete calling functions
    Delete calling functions

--- Final_Project.py
+++ Final_Project.py
@@ -381,18 +381,4 @@ def returnproperty(density, modulus, strength, classification):
     axs.table(cellText= data,rowLabels=rows, rowColours=colors, colLabels=columns, loc='center')
     plt.title("Properties of All Materials")
     plt.show()
-
     
-
-#Call functions
-den, mod, stren, classif, prop_array = readDataFile("data.csv")
-den, mod, stren, prop_array, d_min, d_max, m_min, m_max, s_min, s_max = normalize(den, mod, stren, prop_array)
-test_case = userData(d_min, d_max, m_min, m_max, s_min, s_max)
-graphdata2D(den, mod, stren, classif, test_case)    
-graphdata3D(den, mod, stren, classif, test_case)
-distance_array = distancearray(test_case[0,0], test_case[0,1], test_case[0,2], den, mod, stren)
-finalclass = knearestneighbor(5, test_case[0,0], test_case[0,1], test_case[0,2], den, mod, stren, classif)
-top_materials_array, top_dist_array = topmaterials(finalclass, test_case[0,0], test_case[0,1], test_case[0,2], den, mod, stren, classif)
-first_mat, second_mat, third_mat = returnmaterials(top_materials_array, finalclass, top_dist_array)
-den, mod, stren, prop_array = denormalize(den, mod, stren, prop_array, d_min, d_max, m_min, m_max, s_min, s_max)
-returnproperty(den, mod, stren, classif)
